31e40e975a91d9bb95099543bd08343054bedf01
Nevroz Arslan <nevrozarslan@gmail.com>
2024-09-27 22:07:35 +0300

    feat: add htmx page

--- src/App.cpp
+++ src/App.cpp
@@ -1,11 +1,10 @@
 
+#include "AppComponent.hpp"
 #include "controller/BestMoveController.hpp"
 #include "controller/StaticController.hpp"
 #include "controller/StatisticsController.hpp"
 
 #include "controller/UserController.hpp"
-
-#include "AppComponent.hpp"
 
 #include "oatpp-swagger/Controller.hpp"
 
@@ -13,10 +12,10 @@
 
 #include <iostream>
 
-void run()
-{
+void run() {
 
-  AppComponent components; // Create scope Environment components
+  AppComponent components(
+      const oatpp::base::CommandLineArguments& args); // Create scope Environment components
 
   /* Get router component */
   OATPP_COMPONENT(std::shared_ptr<oatpp::web::server::HttpRouter>, router);
@@ -30,29 +29,54 @@ void run()
   router->addController(std::make_shared<StatisticsController>());
 
   /* Get connection handler component */
-  OATPP_COMPONENT(std::shared_ptr<oatpp::network::ConnectionHandler>, connectionHandler);
+  OATPP_COMPONENT(std::shared_ptr<oatpp::network::ConnectionHandler>, connectionHandler, "http");
 
   /* Get connection provider component */
   OATPP_COMPONENT(std::shared_ptr<oatpp::network::ServerConnectionProvider>, connectionProvider);
 
   /* create server */
   oatpp::network::Server server(connectionProvider, connectionHandler);
-
-  OATPP_LOGd("Server", "Running on port {}...", connectionProvider->getProperty("port").toString())
-
-      server.run();
-
-  /* stop db connection pool */
   OATPP_COMPONENT(std::shared_ptr<oatpp::provider::Provider<oatpp::sqlite::Connection>>,
                   dbConnectionProvider);
-  dbConnectionProvider->stop();
+  OATPP_LOGd("Server", "Running on port {}...", connectionProvider->getProperty("port").toString())
+
+      std::thread serverThread([&server] { server.run(); });
+
+  std::thread pingThread([] {
+    OATPP_COMPONENT(std::shared_ptr<Lobby>, lobby);
+    lobby->runPingLoop(std::chrono::seconds(30));
+  });
+
+  std::thread statThread([] {
+    OATPP_COMPONENT(std::shared_ptr<Statistics>, statistics);
+    statistics->runStatLoop();
+  });
+  OATPP_COMPONENT(oatpp::Object<ConfigDto>, appConfig);
+  if(appConfig->useTLS) {
+    OATPP_LOGi("chesskk",
+               "clients are expected to connect at https://{}:{}/",
+               appConfig->host,
+               appConfig->port);
+  }
+  else {
+    OATPP_LOGi("chesskk",
+               "clients are expected to connect at http://{}:{}/",
+               appConfig->host,
+               appConfig->port);
+  }
+
+  OATPP_LOGi("chesskk", "canonical base URL={}", appConfig->getCanonicalBaseUrl());
+  OATPP_LOGi("chesskk", "statistics URL={}", appConfig->getStatsUrl());
+
+  serverThread.join();
+  pingThread.join();
+  statThread.join();
 }
 
 /**
  *  main
  */
-int main(int argc, const char* argv[])
-{
+int main(int argc, const char* argv[]) {
 
   oatpp::Environment::init();
 
--- src/AppComponent.hpp
+++ src/AppComponent.hpp
@@ -10,23 +10,73 @@
 
 #include <oatpp/web/mime/ContentMappers.hpp>
 #include <oatpp/web/server/HttpConnectionHandler.hpp>
-#include <oatpp/web/server/HttpRouter.hpp>
 
 #include <oatpp/network/tcp/server/ConnectionProvider.hpp>
 
 #include <oatpp/json/ObjectMapper.hpp>
 
 #include <oatpp/web/server/AsyncHttpConnectionHandler.hpp>
+#include <oatpp/web/server/HttpRouter.hpp>
 
 #include "utils/Statistics.hpp"
+#include <oatpp/base/CommandLineArguments.hpp>
 #include <oatpp/macro/component.hpp>
-
 /**
  *  Class which creates and holds Application components and registers components in oatpp::base::Environment
  *  Order of components initialization is from top to bottom
  */
 class AppComponent {
+
+  private:
+  oatpp::base::CommandLineArguments m_cmdArgs;
+
   public:
+  AppComponent(const oatpp::base::CommandLineArguments& cmdArgs)
+      : m_cmdArgs(cmdArgs) { }
+
+  public:
+  /**
+   * Create config component
+   */
+  OATPP_CREATE_COMPONENT(oatpp::Object<ConfigDto>, appConfig)
+  ([this] {
+    auto config = ConfigDto::createShared();
+
+    config->host = std::getenv("EXTERNAL_ADDRESS");
+    if(!config->host) {
+      config->host = m_cmdArgs.getNamedArgumentValue("--host", "localhost");
+    }
+
+    const char* portText = std::getenv("EXTERNAL_PORT");
+    if(!portText) {
+      portText = m_cmdArgs.getNamedArgumentValue("--port", "8443");
+    }
+
+    bool success;
+    auto port = oatpp::utils::Conversion::strToUInt32(portText, success);
+    if(!success || port > 65535) {
+      throw std::runtime_error("Invalid port!");
+    }
+    config->port = (v_uint16)port;
+
+    // config->tlsPrivateKeyPath = std::getenv("TLS_FILE_PRIVATE_KEY");
+    // if(!config->tlsPrivateKeyPath) {
+    //   config->tlsPrivateKeyPath = m_cmdArgs.getNamedArgumentValue("--tls-key", "" CERT_PEM_PATH);
+    // }
+
+    // config->tlsCertificateChainPath = std::getenv("TLS_FILE_CERT_CHAIN");
+    // if(!config->tlsCertificateChainPath) {
+    //   config->tlsCertificateChainPath =
+    //       m_cmdArgs.getNamedArgumentValue("--tls-chain", "" CERT_CRT_PATH);
+    // }
+
+    config->statisticsUrl = std::getenv("URL_STATS_PATH");
+    if(!config->statisticsUrl) {
+      config->statisticsUrl = m_cmdArgs.getNamedArgumentValue("--url-stats", "admin/stats.json");
+    }
+
+    return config;
+  }());
   /**
    *  Swagger component
    */
@@ -46,7 +96,6 @@ class AppComponent {
         4 /* Data-Processing threads */, 1 /* I/O threads */, 1 /* Timer threads */
     );
   }());
-
   /**
    * Create ObjectMapper component to serialize/deserialize DTOs in Controller's API
    */
@@ -59,6 +108,16 @@ class AppComponent {
     mappers->putMapper(json);
 
     return mappers;
+  }());
+
+  /**
+   *  Create ObjectMapper component to serialize/deserialize DTOs in Contoller's API
+   */
+  OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::data::mapping::ObjectMapper>, apiObjectMapper)
+  ([] {
+    auto mapper = std::make_shared<oatpp::json::ObjectMapper>();
+    mapper->serializerConfig().mapper.includeNullFields = false;
+    return mapper;
   }());
 
   /**
