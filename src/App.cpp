114ba7e788af4551068f1c2d2087d1418070d67c
Nevroz Arslan <nevrozarslan@gmail.com>
2024-09-29 01:04:28 +0300

    feat: add htmx page

--- src/App.cpp
+++ src/App.cpp
@@ -1,32 +1,20 @@
+#include "./AppComponent.hpp"
+#include "./controller/MyController.hpp"
 
-#include "AppComponent.hpp"
-#include "controller/BestMoveController.hpp"
-#include "controller/StaticController.hpp"
-#include "controller/StatisticsController.hpp"
-
-#include "controller/UserController.hpp"
-
-#include "oatpp-swagger/Controller.hpp"
-
-#include "oatpp/network/Server.hpp"
+#include <oatpp/network/Server.hpp>
 
 #include <iostream>
 
 void run() {
 
-  AppComponent components(
-      const oatpp::base::CommandLineArguments& args); // Create scope Environment components
+  /* Register Components in scope of run() method */
+  AppComponent components;
 
   /* Get router component */
   OATPP_COMPONENT(std::shared_ptr<oatpp::web::server::HttpRouter>, router);
 
-  oatpp::web::server::api::Endpoints docEndpoints;
-
-  docEndpoints.append(router->addController(UserController::createShared())->getEndpoints());
-  docEndpoints.append(router->addController(BestMoveController::createShared())->getEndpoints());
-  router->addController(oatpp::swagger::Controller::createShared(docEndpoints));
-  router->addController(StaticController::createShared());
-  router->addController(std::make_shared<StatisticsController>());
+  /* Create MyController and add all of its endpoints to router */
+  router->addController(std::make_shared<MyController>());
 
   /* Get connection handler component */
   OATPP_COMPONENT(std::shared_ptr<oatpp::network::ConnectionHandler>, connectionHandler, "http");
@@ -34,59 +22,22 @@ void run() {
   /* Get connection provider component */
   OATPP_COMPONENT(std::shared_ptr<oatpp::network::ServerConnectionProvider>, connectionProvider);
 
-  /* create server */
+  /* Create server which takes provided TCP connections and passes them to HTTP connection handler */
   oatpp::network::Server server(connectionProvider, connectionHandler);
-  OATPP_COMPONENT(std::shared_ptr<oatpp::provider::Provider<oatpp::sqlite::Connection>>,
-                  dbConnectionProvider);
-  OATPP_LOGd("Server", "Running on port {}...", connectionProvider->getProperty("port").toString())
 
-      std::thread serverThread([&server] { server.run(); });
-
-  std::thread pingThread([] {
-    OATPP_COMPONENT(std::shared_ptr<Lobby>, lobby);
-    lobby->runPingLoop(std::chrono::seconds(30));
-  });
-
-  std::thread statThread([] {
-    OATPP_COMPONENT(std::shared_ptr<Statistics>, statistics);
-    statistics->runStatLoop();
-  });
-  OATPP_COMPONENT(oatpp::Object<ConfigDto>, appConfig);
-  if(appConfig->useTLS) {
-    OATPP_LOGi("chesskk",
-               "clients are expected to connect at https://{}:{}/",
-               appConfig->host,
-               appConfig->port);
-  }
-  else {
-    OATPP_LOGi("chesskk",
-               "clients are expected to connect at http://{}:{}/",
-               appConfig->host,
-               appConfig->port);
-  }
-
-  OATPP_LOGi("chesskk", "canonical base URL={}", appConfig->getCanonicalBaseUrl());
-  OATPP_LOGi("chesskk", "statistics URL={}", appConfig->getStatsUrl());
-
-  serverThread.join();
-  pingThread.join();
-  statThread.join();
+  /* Priny info about server port */
+  //OATPP_LOGi(
+  //    "MyApp", "Server running on port='{}'", connectionProvider->getProperty("port").getData());
+  OATPP_LOGi("MyApp", "Server running on port={}", 8000);
+  /* Run server */
+  server.run();
 }
 
-/**
- *  main
- */
 int main(int argc, const char* argv[]) {
 
   oatpp::Environment::init();
 
   run();
-
-  /* Print how many objects were created during app running, and what have left-probably leaked */
-  /* Disable object counting for release builds using '-D OATPP_DISABLE_ENV_OBJECT_COUNTERS' flag for better performance */
-  std::cout << "\nEnvironment:\n";
-  std::cout << "objectsCount = " << oatpp::Environment::getObjectsCount() << "\n";
-  std::cout << "objectsCreated = " << oatpp::Environment::getObjectsCreated() << "\n\n";
 
   oatpp::Environment::destroy();
 
--- src/AppComponent.hpp
+++ src/AppComponent.hpp
@@ -1,92 +1,22 @@
-
 #ifndef AppComponent_hpp
 #define AppComponent_hpp
 
-#include "DatabaseComponent.hpp"
-#include "SwaggerComponent.hpp"
-
-#include "ErrorHandler.hpp"
-#include "ws/Lobby.hpp"
-
-#include <oatpp/web/mime/ContentMappers.hpp>
-#include <oatpp/web/server/HttpConnectionHandler.hpp>
+#include "websocket/WSListener.hpp"
 
 #include <oatpp/network/tcp/server/ConnectionProvider.hpp>
-
-#include <oatpp/json/ObjectMapper.hpp>
-
 #include <oatpp/web/server/AsyncHttpConnectionHandler.hpp>
 #include <oatpp/web/server/HttpRouter.hpp>
 
-#include "utils/Statistics.hpp"
-#include <oatpp/base/CommandLineArguments.hpp>
+#include <oatpp/json/ObjectMapper.hpp>
+
 #include <oatpp/macro/component.hpp>
+
 /**
  *  Class which creates and holds Application components and registers components in oatpp::base::Environment
  *  Order of components initialization is from top to bottom
  */
 class AppComponent {
-
-  private:
-  oatpp::base::CommandLineArguments m_cmdArgs;
-
   public:
-  AppComponent(const oatpp::base::CommandLineArguments& cmdArgs)
-      : m_cmdArgs(cmdArgs) { }
-
-  public:
-  /**
-   * Create config component
-   */
-  OATPP_CREATE_COMPONENT(oatpp::Object<ConfigDto>, appConfig)
-  ([this] {
-    auto config = ConfigDto::createShared();
-
-    config->host = std::getenv("EXTERNAL_ADDRESS");
-    if(!config->host) {
-      config->host = m_cmdArgs.getNamedArgumentValue("--host", "localhost");
-    }
-
-    const char* portText = std::getenv("EXTERNAL_PORT");
-    if(!portText) {
-      portText = m_cmdArgs.getNamedArgumentValue("--port", "8443");
-    }
-
-    bool success;
-    auto port = oatpp::utils::Conversion::strToUInt32(portText, success);
-    if(!success || port > 65535) {
-      throw std::runtime_error("Invalid port!");
-    }
-    config->port = (v_uint16)port;
-
-    // config->tlsPrivateKeyPath = std::getenv("TLS_FILE_PRIVATE_KEY");
-    // if(!config->tlsPrivateKeyPath) {
-    //   config->tlsPrivateKeyPath = m_cmdArgs.getNamedArgumentValue("--tls-key", "" CERT_PEM_PATH);
-    // }
-
-    // config->tlsCertificateChainPath = std::getenv("TLS_FILE_CERT_CHAIN");
-    // if(!config->tlsCertificateChainPath) {
-    //   config->tlsCertificateChainPath =
-    //       m_cmdArgs.getNamedArgumentValue("--tls-chain", "" CERT_CRT_PATH);
-    // }
-
-    config->statisticsUrl = std::getenv("URL_STATS_PATH");
-    if(!config->statisticsUrl) {
-      config->statisticsUrl = m_cmdArgs.getNamedArgumentValue("--url-stats", "admin/stats.json");
-    }
-
-    return config;
-  }());
-  /**
-   *  Swagger component
-   */
-  SwaggerComponent swaggerComponent;
-
-  /**
-   * Database component
-   */
-  DatabaseComponent databaseComponent;
-
   /**
    * Create Async Executor
    */
@@ -96,35 +26,7 @@ class AppComponent {
         4 /* Data-Processing threads */, 1 /* I/O threads */, 1 /* Timer threads */
     );
   }());
-  /**
-   * Create ObjectMapper component to serialize/deserialize DTOs in Controller's API
-   */
-  OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::web::mime::ContentMappers>, apiContentMappers)
-  ([] {
-    auto json = std::make_shared<oatpp::json::ObjectMapper>();
-    json->serializerConfig().json.useBeautifier = true;
 
-    auto mappers = std::make_shared<oatpp::web::mime::ContentMappers>();
-    mappers->putMapper(json);
-
-    return mappers;
-  }());
-
-  /**
-   *  Create ObjectMapper component to serialize/deserialize DTOs in Contoller's API
-   */
-  OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::data::mapping::ObjectMapper>, apiObjectMapper)
-  ([] {
-    auto mapper = std::make_shared<oatpp::json::ObjectMapper>();
-    mapper->serializerConfig().mapper.includeNullFields = false;
-    return mapper;
-  }());
-
-  /**
-   *  Create statistics object
-   */
-  OATPP_CREATE_COMPONENT(std::shared_ptr<Statistics>, statistics)
-  ([] { return std::make_shared<Statistics>(); }());
   /**
    *  Create ConnectionProvider component which listens on the port
    */
@@ -146,16 +48,19 @@ class AppComponent {
    */
   OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::network::ConnectionHandler>,
                          serverConnectionHandler)
-  ([] {
+  ("http", [] {
     OATPP_COMPONENT(std::shared_ptr<oatpp::web::server::HttpRouter>,
                     router); // get Router component
-    OATPP_COMPONENT(std::shared_ptr<oatpp::web::mime::ContentMappers>,
-                    contentMappers); // get ContentMappers component
-
-    auto connectionHandler = oatpp::web::server::HttpConnectionHandler::createShared(router);
-    connectionHandler->setErrorHandler(std::make_shared<ErrorHandler>(contentMappers));
-    return connectionHandler;
+    OATPP_COMPONENT(std::shared_ptr<oatpp::async::Executor>,
+                    executor); // get Async executor component
+    return oatpp::web::server::AsyncHttpConnectionHandler::createShared(router, executor);
   }());
+
+  /**
+   *  Create ObjectMapper component to serialize/deserialize DTOs in Contoller's API
+   */
+  OATPP_CREATE_COMPONENT(std::shared_ptr<oatpp::data::mapping::ObjectMapper>, apiObjectMapper)
+  ([] { return std::make_shared<oatpp::json::ObjectMapper>(); }());
 
   /**
    *  Create websocket connection handler
@@ -165,7 +70,7 @@ class AppComponent {
   ("websocket", [] {
     OATPP_COMPONENT(std::shared_ptr<oatpp::async::Executor>, executor);
     auto connectionHandler = oatpp::websocket::AsyncConnectionHandler::createShared(executor);
-    connectionHandler->setSocketInstanceListener(std::make_shared<Lobby>());
+    connectionHandler->setSocketInstanceListener(std::make_shared<WSInstanceListener>());
     return connectionHandler;
   }());
 };
